03399c48adefe9504165406eafff10f6674fec1b
Lakshmi pavani <96370043+Lakshmipavani123@users.noreply.github.com>
2022-06-22 21:55:20 +0530

    import java.util.Scanner; class mmmm {     public static float findTotalbill(int units)     {         float amt;         if(units<200)         amt=units*1.20f;         else if(units>=200&&units<400)         amt=units*1.50f;         else if(units>=400&&units<600)         amt=units*1.80f;         else         amt=units*2.00f;         if(amt>400)         amt=amt+0.15f*amt;         else         amt=amt+100;         return amt;     }     public static void main(String args[])     {         String id,name;         int units;         float bill;         Scanner sc=new Scanner(System.in);         id=sc.nextLine();         name=sc.nextLine();         units=sc.nextInt();         bill=mmmm.findTotalbill(units);         System.out.format("%.2f",bill);
        }
    }

---
+++ Electricity_Bill.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class mmmm
+{
+    public static float findTotalbill(int units)
+    {
+        float amt;
+        if(units<200)
+        amt=units*1.20f;
+        else if(units>=200&&units<400)
+        amt=units*1.50f;
+        else if(units>=400&&units<600)
+        amt=units*1.80f;
+        else
+        amt=units*2.00f;
+        if(amt>400)
+        amt=amt+0.15f*amt;
+        else
+        amt=amt+100;
+        return amt;
+    }
+    public static void main(String args[])
+    {
+        String id,name;
+        int units;
+        float bill;
+        Scanner sc=new Scanner(System.in);
+        id=sc.nextLine();
+        name=sc.nextLine();
+        units=sc.nextInt();
+        bill=mmmm.findTotalbill(units);
+        System.out.format("%.2f",bill);
+        
+    }
+}
